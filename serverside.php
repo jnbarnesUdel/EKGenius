650b6ba422b5198d0e99428195b91bdcc9476849
jnbarnesUdel <jnbarnes@udel.edu>
2019-10-24 17:49:16 -0400

    Update serverside.php

--- serverside.php
+++ serverside.php
@@ -14,30 +14,11 @@ $fullStr = str_replace(']', "", $fullStr);
 $fullStr = str_replace(' ', "", $fullStr);
 $fullStr = str_replace(',', "", $fullStr);
 
-//get subracks
-$racks = [];
-for($i = 0; $i < pow(2, strlen($fullStr)); $i++){
-	$ans = "";
-	for($j = 0; $j < strlen($fullStr); $j++){
-		//if the jth digit of i is 1 then include letter
-		if (($i >> $j) % 2) {
-		  $ans .= $fullStr[$j];
-		}
-	}
-	if (strlen($ans) > 1){
-  	    $racks[] = $ans;	
-	}
-}
-$racks = array_unique($racks);
-
-$words = "";
-foreach ($racks as $var){
-$var = '"'.$var.'"';
 $dbhandle = new PDO("sqlite:scrabble.sqlite") or die("Failed to open DB");
 if (!$dbhandle) die ($error);
 
 
-$query = "SELECT rack, words FROM racks WHERE rack = $var ";
+$query = "SELECT rack, words FROM racks WHERE rack = $fullStr ";
 
 
 $statement = $dbhandle->prepare($query);
@@ -49,13 +30,8 @@ $statement->execute();
 //this will naturally create a pleasant array of JSON data when I echo in a couple lines
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
 // print_r($results);
-foreach ($results as $parts){
-    foreach ($parts as $stuff){
-            $words .= $stuff;
-            $words .= ",";
-        }
-    }
-}
+
+
 
 //this part is perhaps overkill but I wanted to set the HTTP headers and status code
 //making to this line means everything was great with this request
@@ -63,4 +39,4 @@ header('HTTP/1.1 200 OK');
 //this lets the browser know to expect json
 header('Content-Type: application/json');
 //this creates json and gives it back to the browser
-echo json_encode($words);
+echo json_encode($results);
