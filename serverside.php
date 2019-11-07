0d60c8c676d015f29f29095b2d0cb9144298200b
jnbarnesUdel <jnbarnes@udel.edu>
2019-11-07 16:51:41 -0500

    Update serverside.php

--- serverside.php
+++ serverside.php
@@ -21,9 +21,9 @@ if($count != 4){
 }
 else{
     $id = $str_json[0]->id;
-    $firstName = $str_json[1]->firstName;
-    $lastName = $str_json[2]->lastName;
-    $illness = $str_json[3]->illness
+    $firstName = 0 //$str_json[1]->firstName;
+    $lastName = 0 //$str_json[2]->lastName;
+    $illness = 0 //$str_json[3]->illness
 }
 
 $dbhandle = new PDO("sqlite:test2.sql") or die("Failed to open DB");
