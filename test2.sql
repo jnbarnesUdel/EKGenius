1e49c166ecf2437abec9d3a6c7b3a9e20697f6a5
jnbarnesUdel <jnbarnes@udel.edu>
2019-11-08 18:53:58 -0500

    Delete test2.sql

--- test2.sql
+++
@@ -1,5 +0,0 @@
-PRAGMA foreign_keys=OFF;
-BEGIN TRANSACTION;
-CREATE TABLE patientData(id integer, firstName text, lastName text, illness text);
-INSERT INTO "patientData" VALUES(1,'John','Doe','Weak');
-COMMIT;
